8c77a71e41c55337330914a12f5c1d869bf6474b
Krishan <krishanchopra30@yahoo.com>
2018-10-21 11:51:11 -0700

    Create an intent and pass the 2nd activity

--- app/src/main/java/edu/osucascades/choprakrishanexampractice/MainActivity.java
+++ app/src/main/java/edu/osucascades/choprakrishanexampractice/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.osucascades.choprakrishanexampractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,24 +8,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mMangleButton;
-    EditText mNameInput;
+    private EditText mFirstNameInput;
+
+    private String[] lastNames = new String[] {
+            "Smith",
+            "Carter",
+            "Scott",
+            "Doe",
+            "Graham"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mNameInput = (EditText) findViewById(R.id.mangle_button_id);
+        mFirstNameInput = (EditText) findViewById(R.id.first_name_input);
 
         mMangleButton = (Button) findViewById(R.id.mangle_button_id);
         mMangleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstName = mNameInput.getText().toString();
-                Toast.makeText(MainActivity.this, firstName, Toast.LENGTH_SHORT).show();
+
+                if (mFirstNameInput.length() <= 0) {
+                    Toast.makeText(MainActivity.this, "Name field is empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, ManglerActivity.class);
+                    intent.putExtra("FirstName", mFirstNameInput.getText().toString());
+                    startActivity(intent);
+                    //Random random = new Random();
+                    //int randomNumber = random.nextInt(4+1);
+                    //Toast.makeText(MainActivity.this, mFirstNameInput.getText() + " " + lastNames[randomNumber], Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/edu/osucascades/choprakrishanexampractice/ManglerActivity.java
+++ app/src/main/java/edu/osucascades/choprakrishanexampractice/ManglerActivity.java
@@ -2,12 +2,44 @@ package edu.osucascades.choprakrishanexampractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class ManglerActivity extends AppCompatActivity {
+
+    private Button mRemangleButton;
+    private Button mResetButton;
+    private TextView mMangleName;
+
+    private String[] lastNames = new String[] {
+            "Smith",
+            "Carter",
+            "Scott",
+            "Doe",
+            "Graham"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mangler);
+
+        final String firstName = getIntent().getStringExtra("FirstName");
+        String mangleName = firstName + ' ' + lastNames[(int)(Math.random() * lastNames.length)];
+
+        mMangleName = (TextView) findViewById(R.id.first_name_mangle);
+        mMangleName.setText(mangleName);
+
+        mRemangleButton = (Button) findViewById(R.id.remangle_button_id);
+        mRemangleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newMangleName = firstName + ' ' + lastNames[(int)(Math.random() * lastNames.length)];
+                mMangleName.setText(newMangleName);
+            }
+        });
     }
 }
